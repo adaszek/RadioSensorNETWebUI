025f7c78afea26560042e315fdef50167ee48707
adaszek <adaszek@gmail.com>
2017-02-25 14:34:29 +0100

    Drawing diagram for a device works fine

--- app.js
+++ app.js
@@ -11,6 +11,47 @@ var devices = require('./routes/devices');
 var locations = require('./routes/locations');
 
 var app = express();
+var socket_io = require("socket.io");
+var io = socket_io();
+app.io = io;
+
+
+var redis = require("redis");
+var bluebird = require("bluebird");
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+var client = redis.createClient(6379, "192.168.1.158")
+
+client.onAsync("connect", () => {
+    console.log("connected")
+})
+
+io.on('connection', function(socket) {
+    console.log("a user connected");
+    client.smembersAsync("sensors").then((object) => {
+        var options = {};
+        for(var sid in object) {
+            options[object[sid]] = object[sid];
+        }
+        socket.emit("sensor_list", options);
+    });
+
+    socket.on("data_request", function(data) {
+        client.hgetallAsync("sensor:" + data.sid + ":" + data.mid).then((object) => {
+            socket.emit('data_response', object);
+        }).catch((e) => {
+            console.log("error")
+            console.log(e)
+        });
+    });
+
+    socket.on("disconnect", function() {
+        console.log("user disconnected");
+    });
+    
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
