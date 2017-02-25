f5c1aacd0dcfab00ea49eeae74e97b560fa8ec22
adaszek <adaszek@gmail.com>
2017-02-25 11:19:23 +0100

    Started refactoring of html file to pug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var devices = require('./routes/devices');
+var locations = require('./routes/locations');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/devices', devices);
+app.use('/locations', locations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
