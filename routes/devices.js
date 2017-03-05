5d7a846cbd367cde8784f0ca29a73645a5ba24de
adaszek <adaszek@gmail.com>
2017-03-05 14:02:52 +0100

    Working on devices table initial data and latest activity

--- routes/devices.js
+++ routes/devices.js
@@ -1,8 +1,85 @@
+var bluebird = require("bluebird");
+
+function get_latest_activity(redis_client) {
+    return redis_client
+        .multi()
+        .smembers("sensors")
+        .hgetall("functions")
+        .execAsync()
+        .then(data => {
+            return data;
+            /*
+            var funct = [];
+            for (var i in data) {
+                funct.push(data[i]);
+            }
+            var sid = "sensor:" + sensor_id + ":";
+
+            return bluebird.map(funct, (func) => {
+                return redis
+                    .zrangeAsync(sid + func + ":timestamps", -1, -1);
+            });
+            */
+        });
+}
+
+function get_init_data(redis_client) {
+    return redis_client
+        .multi()
+        .smembers("sensors")
+        .hgetall("sensors:functions")
+        .hgetall("sensors:last_location")
+        .hgetall("functions")
+        .execAsync()
+        .then((data) => {
+            var return_array = [];
+            for(var i in data[0]) {
+                var row = [];
+                row.push(data[0][i]);
+
+                if (data[1].hasOwnProperty(data[0][i])) {
+                    var to_parse = data[1][data[0][i]];
+                    var temp_split = to_parse.split(";");
+                    var temp_object = {};
+                    for (var param in temp_split) {
+                        var arg_and_value = temp_split[param].split(":");
+                        if (arg_and_value[0] === "r" || arg_and_value[0] === "w") {
+                            var properties = arg_and_value[1].split(",");
+                            var result = [];
+                            if(properties.length > 1) {
+                                result = properties.map((x) => {
+                                return data[3][x];
+                                });
+                            }
+                            temp_object[arg_and_value[0]] = result;
+                        } else {
+                            temp_object[arg_and_value[0]] = arg_and_value[1];
+                        }
+                    }
+                    row.push(temp_object);
+                } else {
+                    row.push("unknown");
+                }
+                if (data[2].hasOwnProperty(data[0][i])) {
+                    row.push(data[2][data[0][i]]);
+                } else {
+                    row.push("unknown");
+                }
+
+                return_array.push(row);
+            }
+            return return_array;
+        })
+        .catch(e => {
+            console.log(e);
+        });
+}
+
+
 module.exports = () => {
-    var redis = require("redis");
-    var bluebird = require("bluebird");
     var express = require('express');
     var router = express.Router();
+    var redis = require("redis");
 
     bluebird.promisifyAll(redis.RedisClient.prototype);
     bluebird.promisifyAll(redis.Multi.prototype);
@@ -19,54 +96,12 @@ module.exports = () => {
     });
 
     router.get('/api', function(req, res, next) {
-        client
-            .multi()
-            .smembers("sensors")
-            .hgetall("sensors:functions")
-            .hgetall("sensors:last_location")
-            .hgetall("functions")
-            .execAsync()
-            .then((data) => {
-                console.log(data);
-                var return_array = [];
-                for(var i in data[0]) {
-                    var row = [];
-                    row.push(data[0][i]);
-
-                    if (data[1].hasOwnProperty(data[0][i])) {
-                        var to_parse = data[1][data[0][i]];
-                        var temp_split = to_parse.split(";");
-                        var temp_object = {};
-                        for (var param in temp_split) {
-                            var arg_and_value = temp_split[param].split(":");
-                            if (arg_and_value[0] === "r" || arg_and_value[0] === "w") {
-                                var properties = arg_and_value[1].split(",");
-                                var result = [];
-                                if(properties.length > 1) {
-                                    result = properties.map((x) => {
-                                    return data[3][x];
-                                    });
-                                }
-                                temp_object[arg_and_value[0]] = result;
-                            } else {
-                            temp_object[arg_and_value[0]] = arg_and_value[1];
-                            }
-                        }
-                        row.push(temp_object);
-                    } else {
-                        row.push("unknown");
-                    }
-                    if (data[2].hasOwnProperty(data[0][i])) {
-                        row.push(data[2][data[0][i]]);
-                    } else {
-                        row.push("unknown");
-                    }
-
-                    return_array.push(row);
-                }
-                console.log(JSON.stringify(return_array));
-                res.send({ data: return_array });
+        get_init_data(client).then(init_data => {
+            get_latest_activity(client).then(latest_activity => {
+                console.log(latest_activity);
             });
+            res.send({ data: init_data });
+        });
     });
 
     return router;
