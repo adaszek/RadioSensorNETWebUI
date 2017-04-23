822c0d2a1eb6c69b08f297cf41c84776fc7da932
adaszek <adaszek@gmail.com>
2017-04-23 12:07:07 +0200

    Diagram updates its range basing on datepicker selected date

--- public/javascripts/get_device_data.js
+++ public/javascripts/get_device_data.js
@@ -1,19 +1,36 @@
 var socket;
 
 function ask_for_data(socket, sensor_id, measurement_id, from, to) {
-    socket.emit('data_request', {
-        "sid": sensor_id,
-        "mid": measurement_id,
-        "from": from,
-        "to": to
-    });
+    if(from <= to) {
+        socket.emit('data_request', {
+            "sid": sensor_id,
+            "mid": measurement_id,
+            "from": from,
+            "to": to
+        });
+    }
 }
 
-function data_req() {
-}
 
 $(document).ready(function () {
     socket = io();
+    $('#datetimepicker1').datetimepicker({
+        locale: 'en-gb',
+        format: 'YYYY/MM/DD HH:mm',
+        showTodayButton: true,
+        defaultDate: Date.now() - 60000 * 60 * 2,
+        showClear: true
+    });
+
+    $('#datetimepicker2').datetimepicker({
+        locale: 'en-gb',
+        format: 'YYYY/MM/DD HH:mm',
+        showTodayButton: true,
+        useCurrent: true,
+        defaultDate: Date.now(),
+        showClear: true
+    });
+
 
     var g3 = new Dygraph(document.getElementById("graphdiv3"), [], {
         legend: "follow",
@@ -26,10 +43,11 @@ $(document).ready(function () {
 
     socket.on('data_response', function(data) {
         var graph_data = data[0].map((n, index) => [new Date(parseInt(n) * 1000), parseFloat(data[1][index])]);
-        console.log(graph_data)
         g3.updateOptions({
             'file': graph_data,
         });
     });
+    
+    //setInterval(ask_for_data, 5000, socket, #{device_id}, '#{measurement_id}');
 
 });
